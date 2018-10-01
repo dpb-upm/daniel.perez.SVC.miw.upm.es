823bac9b41256ff0b59a8bbfd7d5bc3d446308be
Daniel Pérez <>
2018-10-01 19:26:11 +0200

    issue#05 Crear la clase C51Test

---
+++ src/test/java/es/upm/miw/iwvg/forge/junit/C51Test.java
@@ -0,0 +1,26 @@
+package es.upm.miw.iwvg.forge.junit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class C51Test {
+
+    private C51 c51;
+
+    @BeforeEach
+    void before() {
+        this.c51 = new C51();
+    }
+
+    @Test
+    void testM1() {
+        assertEquals("m1", this.c51.m1());
+    }
+
+    @Test
+    void testM2() {
+        assertEquals("m2", this.c51.m2());
+    }
+}
